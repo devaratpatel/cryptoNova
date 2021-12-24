afc8d530e4cf772ddac9887693ada83fb9a40739
Devarat Patel <devaratpatel1@gmail.com>
2021-12-24 17:59:31 -0500

    feat: added loading data functionality

--- src/components/CoinList.jsx
+++ src/components/CoinList.jsx
@@ -5,16 +5,20 @@ import { WatchListContext } from '../context/watchListContext';
 function CoinList() {
   const [coins, setCoins] = useState([]);
   const { watchList } = useContext(WatchListContext);
-  console.log('watchList---->', watchList);
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       const response = await coinGecko.get('/coins/markets', {
         params: {
           vs_currency: 'usd',
           ids: watchList.join(', '),
         },
       });
+      setCoins(response.data);
       console.log('response.data -->', response.data);
+      setIsLoading(false);
     };
     fetchData();
   }, []);
