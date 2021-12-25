dca0087c88a2b5583605ecc773a90b58cd09a68b
Devarat Patel <devaratpatel1@gmail.com>
2021-12-25 13:20:48 -0500

    feat: added dynamic routing

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
           <Header />
           <Routes>
             <Route exact path="/" element={<CoinSummary />} />
+            <Route exact path="/coins/:id" element={<CoinDetails />} />
           </Routes>
         </Router>
       </WatchListContextProvider>
@@ -20,3 +21,25 @@ function App() {
 }
 
 export default App;
+// import React from 'react';
+// import { BrowserRouter, Route } from 'react-router-dom';
+// import { CoinDetails, CoinSummary } from './pages';
+// import Header from './components/Header';
+// import './App.css';
+// import { WatchListContextProvider } from './context/watchListContext';
+
+// const App = () => {
+//   return (
+//     <div className="container">
+//       <WatchListContextProvider>
+//         <BrowserRouter>
+//           <Header />
+//           <Route exact path="/" component={CoinSummary} />
+//           <Route path="/coins/:id" component={CoinDetails} />
+//         </BrowserRouter>
+//       </WatchListContextProvider>
+//     </div>
+//   );
+// };
+
+// export default App;
--- src/pages/CoinDetails.jsx
+++ src/pages/CoinDetails.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
-function CoinDetails() {
+const CoinDetails = () => {
+  const { id } = useParams();
+  console.log('this is cryptocurrency:', id);
   return (
     <div>
       <h5>Coin Details</h5>
     </div>
   );
-}
+};
 
 export default CoinDetails;
