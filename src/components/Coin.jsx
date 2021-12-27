3b8ac5f1204d933306f0b7214dcbfa1fdb60fecd
Devarat Patel <devaratpatel1@gmail.com>
2021-12-27 14:05:12 -0500

    feat: minor styling changes

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -6,7 +6,7 @@ const Coin = ({ coin, deleteCoin }) => {
     <Link to={`/coins/${coin.id}`} className="text-decoration-none my-1 coin">
       <li className="coinlist-item list-group-item list-group-item-action d-flex justify-content-between align-items-center text-dark">
         <img className="coinlist-image" src={coin.image} alt="" />
-        <span className="text-decoration-none">{coin.current_price}</span>
+        <span className="text-decoration-none"> Price: ${coin.current_price}</span>
 
         <span
           className={
@@ -18,7 +18,7 @@ const Coin = ({ coin, deleteCoin }) => {
           ) : (
             <i className="fas fa-sort-up align-middle mr-1"></i>
           )}
-          {coin.price_change_percentage_24h}
+          24H: {coin.price_change_percentage_24h}%
         </span>
         <i
           onClick={e => {
