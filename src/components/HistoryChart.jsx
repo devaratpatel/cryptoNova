a368b207ee1629d61358ea50af5475bf433602e5
Devarat Patel <devaratpatel1@gmail.com>
2021-12-29 10:42:24 -0500

    feat: added back button to HistoryChart

--- src/components/HistoryChart.jsx
+++ src/components/HistoryChart.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useRef, useState } from 'react';
 import Chartjs from 'chart.js';
 import { historyOptions } from '../chartConfigs/chartConfigs';
+import { useNavigate } from 'react-router-dom';
 
 export const HistoryChart = ({ data }) => {
   const chartRef = useRef();
   const { day, week, month, year, detail } = data;
   const [timeFormat, setTimeFormat] = useState('24h');
+  const navigate = useNavigate();
 
   const determineTimeFormat = () => {
     switch (timeFormat) {
@@ -57,30 +59,41 @@ export const HistoryChart = ({ data }) => {
     }
   };
   return (
-    <div className="bg-white border mt-2 rounded p-3">
-      <div>{renderPrice()}</div>
-      <div>
-        <canvas ref={chartRef} id="myChart" width="250" height="250"></canvas>
-      </div>
-      <div className="chart-button mt-1">
-        <button onClick={() => setTimeFormat('24h')} className="btn btn-outline-secondary btn-sm">
-          24h
-        </button>
-        <button
-          onClick={() => setTimeFormat('7d')}
-          className="btn btn-outline-secondary btn-sm mx-1"
-        >
-          7d
-        </button>
-        <button onClick={() => setTimeFormat('1m')} className="btn btn-outline-secondary btn-sm">
-          1m
-        </button>
-        <button
-          onClick={() => setTimeFormat('1y')}
-          className="btn btn-outline-secondary btn-sm mx-1"
-        >
-          1y
-        </button>
+    <div>
+      <button
+        onClick={() => {
+          navigate('/');
+        }}
+        className="btn btn-primary "
+        type="button"
+      >
+        Back
+      </button>
+      <div className="bg-white border mt-2 rounded p-3">
+        <div>{renderPrice()}</div>
+        <div>
+          <canvas ref={chartRef} id="myChart" width="250" height="250"></canvas>
+        </div>
+        <div className="chart-button mt-1">
+          <button onClick={() => setTimeFormat('24h')} className="btn btn-outline-secondary btn-sm">
+            24h
+          </button>
+          <button
+            onClick={() => setTimeFormat('7d')}
+            className="btn btn-outline-secondary btn-sm mx-1"
+          >
+            7d
+          </button>
+          <button onClick={() => setTimeFormat('1m')} className="btn btn-outline-secondary btn-sm">
+            1m
+          </button>
+          <button
+            onClick={() => setTimeFormat('1y')}
+            className="btn btn-outline-secondary btn-sm mx-1"
+          >
+            1y
+          </button>
+        </div>
       </div>
     </div>
   );
